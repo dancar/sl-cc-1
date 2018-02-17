fc97dc820f66c3d4398b47a8b0f6c9db5e2bfa7f
Dan Carmon <dan@Pizote.local>
2018-02-17 11:01:05 +0100

    Removed some boilerplate files

---
+++ src/components/Scatterplot.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Scatterplot extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: undefined
+    }
+  }
+
+  render () {
+    return (
+            'TBD'
+    )
+  }
+}
