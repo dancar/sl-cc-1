6d48b910a2585b0cad09a9f5dbd955c9603f959c
Dan Carmon <dan@Pizote.local>
2018-02-17 16:43:53 +0100

    Initial Scatterplot component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,49 @@ import './App.css'
 
 class App extends Component {
   render () {
+const plotpoints = [
+  {
+    "start_time": "2017-11-29T04:56:12Z",
+    "status": "pass",
+    "duration": 126
+  },
+
+  {
+    "start_time": "2017-11-28T03:22:12Z",
+    "status": "error",
+    "duration": 205
+  },
+
+  {
+    "start_time": "2017-11-28T02:24:12Z",
+    "status": "fail",
+    "duration": 20
+  },
+
+  {
+    "start_time": "2017-11-28T05:24:12Z",
+    "status": "pass",
+    "duration": 90
+  },
+
+  {
+    "start_time": "2017-11-29T06:24:12Z",
+    "status": "error",
+    "duration": 90
+  },
+
+  {
+    "start_time": "2017-11-28T14:12:12Z",
+    "status": "pass",
+    "duration": 200
+  }
+]
     return (
       <div className='App'>
         <header className='App-header'>
           <h1 className='App-title'>Interactive Scatterplot Component</h1>
         </header>
-        <Scatterplot />
+        <Scatterplot data={plotpoints} />
       </div>
     )
   }
--- src/components/Scatterplot.js
+++ src/components/Scatterplot.js
@@ -1,16 +1,132 @@
 import React from 'react'
+import './Scatterplot.css'
+import ReacthEcharts from 'echarts-for-react'
+import Measure from 'react-measure'
+
+const FILL_COLOR_BY_STATUS = {
+  pass: "green",
+  error: "orange",
+  fail: "red"
+}
+
+class Surface extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      dimensions: {
+        width: -1,
+        height: -1
+      }
+    }
+  }
+
+  renderPoints () {
+    const { width, height } = this.state.dimensions
+    const timespan = this.props.maxTime - this.props.minTime
+    const points = this.props.data.map( (point, index) => {
+      const top = (point.duration * height / this.props.maxDuration)
+      const timeOffset = point.timestamp - this.props.minTime
+      const left = timeOffset * width / timespan
+      let pointClassName = "scatterplot-point"
+      if (point.selected) {
+        pointClassName += " scatterplot-point-selected"
+      }
+
+      return (
+        <circle key={"point_" + index}
+                className={pointClassName}
+                cx={left}
+                cy={top}
+                fill={FILL_COLOR_BY_STATUS[point.status]}
+                onClick={() => this.props.onPointClicked(point, index)}
+                r={4} />
+      )
+    })
+    return points
+  }
+
+  render () {
+    const { width, height } = this.state.dimensions
+    return (
+      <Measure
+        bounds
+        onResize={(contentRect) => this.setState({dimensions: contentRect.bounds})}
+        >
+          { ({ measureRef }) =>
+            <svg width={width} height={height} ref={measureRef} className="scatterplot-surface">{ this.renderPoints()}</svg>
+
+            }
+      </Measure>
+    )
+  }
+}
 
 export default class Scatterplot extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {
-      selected: undefined
+    this.state = this.calcState(props)
+  }
+
+  onComponentWillReceiveProps (newProps) {
+    // TODO: optimize for changes?
+    this.setState(this.calcState(newProps))
+  }
+
+  calcState (props) {
+    const data = props.data.map(({start_time, status, duration}) => {
+      const timestamp = Date.parse(start_time)
+      return {timestamp, status, duration}
+    })
+
+    const durations = data.map((item) => item.duration)
+    const maxDuration = Math.max(...durations)
+    const minDuration = Math.min(...durations)
+
+    const timestamps = data.map((item) => item.timestamp)
+    const maxTime = Math.max(...timestamps)
+    const minTime = Math.min(...timestamps)
+
+    return {
+      maxDuration, minDuration,
+      maxTime, minTime,
+      data
     }
+
+  }
+
+  handlePointClicked (point, pointIndex) {
+    console.log('<-DANDEBUG-> Scatterplot.js\\ 98: arguments:', arguments);
+
+    const data = this.state.data.map( (item, index) => {
+      if (pointIndex === index) {
+        return {
+          ...item,
+          selected: !item.selected
+        }
+      }
+      return item
+    })
+    this.setState({data})
   }
 
   render () {
     return (
-            'TBD'
+      <div className="scatterplot-container">
+        <div className="scatterplot-yaxis"></div>
+        <Surface data={this.state.data}
+                 maxDuration={this.state.maxDuration}
+                 minTime={this.state.minTime}
+                 maxTime={this.state.maxTime}
+                 onPointClicked={this.handlePointClicked.bind(this)}
+                 />
+        <div className="scatterplot-corner"></div>
+        <div className="scatterplot-xaxis">
+          <div className="scatterplot-x-label" style={{left: "10%"}}>
+            bla
+          </div>
+
+        </div>
+      </div>
     )
   }
 }
